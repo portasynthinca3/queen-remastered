45f9cb4c31cae59e54d295e8a9faf2a216a19c60
tarsss <tars4teen@gmail.com>
2021-01-08 06:03:15 +0300

    did stuff idk

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Entity : NetworkBehaviour
+{
+    new public string name;
+    public string description;
+
+    public float health;
+    public float maxHealth;
+}
---
+++ Assets/Scripts/EntitySelector.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntitySelector : MonoBehaviour
+{
+    private List<Entity> selectedEntities = new List<Entity>();
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = Camera.main;    
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Entity entity = RaycastForEntity();
+            DeselectAll();
+            selectedEntities.Add(entity);
+        }
+    }
+
+    public void DeselectAll()
+    {
+        selectedEntities.Clear();
+    }
+
+    private Entity RaycastForEntity()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+        if(hit.collider != null)
+        {
+            Entity entity = hit.collider.gameObject.GetComponent<Entity>();
+
+            if (entity != null) return entity;
+        }
+        
+        return null;
+    }
+}
